0539611e9a9ae9f4fa4b64dff6ad35613a8eb9aa
Maia Kamyab <mahtakamyab@gmail.com>
2016-12-05 21:39:38 -0800

    add faq + menu + modify styles

--- scripts.js
+++ scripts.js
@@ -6,4 +6,16 @@ $(document).ready(function() {
     $('.directions-map').on('mouseleave', function() {
         $('.directions-map iframe').css('pointer-events', 'none');
     });
+
+    $('.mobile-top-menu-icon').on('click', openMenu);
+
+    $('.mobile-top-menu a.close-menu-icon').on('click', closeMenu);
 });
+
+function openMenu() {
+    $('.mobile-top-menu').addClass('open');
+}
+
+function closeMenu() {
+    $('.mobile-top-menu').removeClass('open');
+}
