9f45e758c114846f7e3c4de3f95786e1aa5fcb76
Maia Kamyab <mahtakamyab@gmail.com>
2017-01-10 18:18:17 -0800

    another attempt to fix the scroll issue

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
+function setCss() {
+    var deviceHeight = screen.height;
+    if (deviceHeight < 769) {
+        $('.fix-height').css('min-height', deviceHeight + 'px');
+        $('.hero').css('height', deviceHeight + 'px');    
+    }
+}
+
 $(document).ready(function() {
     $('.directions-map').on('click', function () {
         $('.directions-map iframe').css('pointer-events', 'auto');
@@ -10,6 +18,10 @@ $(document).ready(function() {
     $('.mobile-top-menu-icon').on('click', openMenu);
 
     $('.mobile-top-menu a.close-menu-icon').on('click', closeMenu);
+
+    // find device height for css properties
+    // scroll gets jumpy if I use vh
+    setCss();
 });
 
 function openMenu() {
